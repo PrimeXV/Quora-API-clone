92c598a12925dcd5691ea2d2765cb96c8b5f4912
Hassan Ganiyu <hassantaiwo554@gmail.com>
2025-03-25 12:53:25 +0100

    backend tweked to work perfectly

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
+const dotenv = require("dotenv");
 const path = require("path");
 const express = require("express");
-const dotenv = require("dotenv");
 const morgan = require("morgan");
 const colors = require("colors");
 const fileupload = require("express-fileupload");
